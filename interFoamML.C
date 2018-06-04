803c42d18d46e3b53c3fc84e79e6c7fb4a8cee79
masonlr <masonlr@unimelb.edu.au>
2018-06-04 13:24:28 +0100

    Add full path to CorrectPhi.H library file

--- interFoamML.C
+++ interFoamML.C
@@ -47,7 +47,7 @@ Description
 #include "turbulentTransportModel.H"
 #include "pimpleControl.H"
 #include "fvOptions.H"
-#include "CorrectPhi.H"
+#include "cfdTools/general/CorrectPhi/CorrectPhi.H"
 #include "fvcSmooth.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
